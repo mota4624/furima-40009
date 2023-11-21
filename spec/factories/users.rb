a6a4140f3ab29ffec7286c28fdd403a4b3756346
mota4624 <rila9246@gmail.com>
2023-11-21 15:05:14 +0900

    Fakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.last_name }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     lastname              { '太郎' }
     firstname             { '山田' }
